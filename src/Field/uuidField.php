66d56f40ae075ec370da80f6220b649554ac293a
zomars <zomars@me.com>
2017-02-16 12:45:53 -0700

    Implement FieldTypeInterface for bolt 3.3 update

--- src/Field/uuidField.php
+++ src/Field/uuidField.php
@@ -2,10 +2,33 @@
 
 namespace Bolt\Extension\Zomars\UuidFieldType\Field;
 
-use Bolt\Field\FieldInterface;
+use Bolt\Storage\EntityManager;
+use Bolt\Storage\Field\Type\FieldTypeBase;
+use Bolt\Storage\QuerySet;
 
-class UuidField implements FieldInterface
+class UuidField extends FieldTypeBase
 {
+
+    public function persist(QuerySet $queries, $entity, EntityManager $em = null)
+    {
+        $key = $this->mapping['fieldname'];
+        $qb = $queries->getPrimary();
+        $value = $entity->get($key);
+
+        $qb->setValue($key, ':' . $key);
+        $qb->set($key, ':' . $key);
+        $qb->setParameter($key, (string)$value);
+    }
+
+    public function hydrate($data, $entity)
+    {
+        $key = $this->mapping['fieldname'];
+
+        $val = isset($data[$key]) ? $data[$key] : null;
+        if ($val !== null) {
+            $this->set($entity, $val);
+        }
+    }
 
     public function getName()
     {
@@ -19,7 +42,7 @@ class UuidField implements FieldInterface
 
     public function getStorageType()
     {
-        return 'text';
+        return 'string';
     }
 
     public function getStorageOptions()
